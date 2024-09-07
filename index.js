d16ff5cd4a1de1dceeea160423c8808c8eddb562
sonu-singh <sonusinghanpcs@gmail.com>
2024-09-08 00:14:19 +0530

    dontevfile

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+require('dotenv').config();
+const PORT = process.env.PORT ;
 
 // Enable CORS for all requests
 app.use(cors());
